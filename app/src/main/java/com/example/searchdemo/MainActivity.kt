17f8ac8fc9dea576c85a5a16dce197a353a42c69
Mahadi Hasan <39445677+ratingminus@users.noreply.github.com>
2022-10-21 18:55:54 +0600

    manifest updated with SearchActivity class

--- app/src/main/java/com/example/searchdemo/MainActivity.kt
+++ app/src/main/java/com/example/searchdemo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.searchdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val changeActivityButton = findViewById<Button>(R.id.changeActivity)
+        changeActivityButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
